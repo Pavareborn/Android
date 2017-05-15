e7bac45d17abb6f95f711ea11cfe6ae0f2ecbeed
Pavareborn <pavanello.paolo.zuccante@gmail.com>
2017-05-15 20:53:26 +0200

    ProvaIntent

---
+++ ProvaIntent/app/src/main/java/pava/provaintent/LoggedActivity.java
@@ -0,0 +1,21 @@
+package pava.provaintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LoggedActivity extends AppCompatActivity {
+
+    TextView loggedUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged);
+
+        loggedUser = (TextView) findViewById(R.id.loggedUser);
+
+        loggedUser.setText("Benvenuto! " + getIntent().getStringExtra("username"));
+
+    }
+}
